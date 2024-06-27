2896a48022a8af4653c1ca27e17c50c663e72951
L1IanBinder <lbinder@lasalleflorida.edu.ar>
2024-06-27 12:56:58 -0300

    Pasó a ser mensaje udp

--- TP3 Márquez-Binder/server.py
+++ TP3 Márquez-Binder/server.py
@@ -1,9 +1,22 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('localhost',12345))
-s.listen()
+import socket
 
-conn, addr = s.accept()
-data = conn.recv(1024)
-conn.sendall('Message for client'.encode())
+# Create a UDP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the address and port
+s.bind(('localhost', 12345))
+
+print("UDP server is up and listening on port 12345")
+
+while True:
+    # Receive data from the client (1024 bytes)
+    data, addr = s.recvfrom(1024)
+    print(f"Received message from {addr}: {data.decode()}")
+
+    # Send a response back to the client
+    response = "Message for client".encode()
+    s.sendto(response, addr)
+
+    print(response)
